913915e6fbd5e888e63ef425345ceb7173813dde
Carlos Alvarenga <c.alvarenga.c7@gmail.com>
2024-10-01 22:37:46 -0600

    Empezando con el HomeScreen

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,14 +1,9 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text>Hola Perro</Text>
     </View>
   );
 }
@@ -18,6 +13,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FFF',
   },
   title: {
     fontSize: 20,
